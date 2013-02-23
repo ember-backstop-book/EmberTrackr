9a4022475e765c29aca3bbb80afc6ac5698162e1
Wesley Workman <wworkman@batterii.com>
2013-02-22 19:16:20 -0500

    Added a gravatar computed property to the user model. The user's gravatar is now displayed on the show user page.

--- app/models/user.js
+++ app/models/user.js
@@ -1,13 +1,28 @@
 App.User = DS.Model.extend({
 	firstName: DS.attr('string'),
 	lastName: DS.attr('string'),
+    email: DS.attr('email'),
     
     displayName: function() {
         return '%@ %@'.fmt(this.get('firstName'), this.get('lastName'));
-    }.property('firstName', 'lastName')
+    }.property('firstName', 'lastName'),
+    
+    gravatarURL: function() {
+        var email = this.get('email'),
+            computedMD5;
+        
+        if(Ember.isEmpty(email)) { 
+            computedMD5 = "00000000000000000000000000000000";
+        } else {
+            computedMD5 = md5(email);
+        }
+            
+        return 'http://www.gravatar.com/avatar/%@.jpg?s=80'.fmt(computedMD5);
+    }.property('email')
 });
 
 App.User.FIXTURES = [
-    { id: 1, firstName: 'Tom', lastName: 'Dale' },
-    { id: 2, firstName: 'Yehuda', lastName: 'Katz' }
-];
+    { id: 1, firstName: 'Tom', lastName: 'Dale', email: 'tom@tomdale.net' },
+    { id: 2, firstName: 'Yehuda', lastName: 'Katz', email: 'wycats@gmail.com' },
+    { id: 3, firstName: 'Peter', lastName: 'Wagenet', email: 'peter.wagenet@gmail.com' }
+]; 
